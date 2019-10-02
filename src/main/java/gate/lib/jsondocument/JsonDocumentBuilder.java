5a611c1057988049146471a06568fa2d4ffb370b
Johann Petrak <johann.petrak@gmail.com>
2019-10-03 01:34:13 +0200

    WIP

--- src/main/java/gate/lib/jsondocument/JsonDocumentBuilder.java
+++ src/main/java/gate/lib/jsondocument/JsonDocumentBuilder.java
@@ -5,8 +5,12 @@
  */
 package gate.lib.jsondocument;
 
-import gateimport java.util.HashSet;
+import com.fasterxml.jackson.jr.ob.JSON;
+import java.util.HashSet;
 import gate.Document;
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
 
 /**
  *
@@ -15,31 +19,55 @@ import gate.Document;
 public class JsonDocumentBuilder {
   Document doc;
   Set<String> includedSets;
+  File toFile = null;
   private JsonDocumentBuilder() {}
   public JsonDocumentBuilder(Document doc) {
     // TODO: for now check that Document is a SimpleDocument
     this.doc = doc;
-    includedSets = new HashSet<String>();
-    for (String annname : doc.getNamedAnnotations()) {
+    includedSets = new HashSet<>();
+    for (String annname : doc.getNamedAnnotationSets().keySet()) {
       includedSets.add(annname);
     }
   }
   
-  public includeAllSets() {
-    for (String annname : doc.getNamedAnnotations()) {
+  public JsonDocumentBuilder includeAllSets() {
+    for (String annname : doc.getNamedAnnotationSets().keySet()) {
       includedSets.add(annname);
     }    
+    return this;
   }
 
-  public excludeAllSets() {
+  public JsonDocumentBuilder excludeAllSets() {
     includedSets.clear();
+    return this;
   }
   
-  public includeSet(String setname) {
+  public JsonDocumentBuilder includeSet(String setname) {
     includedSets.add(setname);
+    return this;
   }
   
-  public excludeSet(String setname) {
+  public JsonDocumentBuilder excludeSet(String setname) {
     includedSets.remove(setname);
+    return this;
   }
+  
+  private JsonDocument buildJsonDoc() {
+    // actually build the jsdondoc and return it, using the current features
+    // TODO: set fields etc
+    return new JsonDocument();
+  }
+  
+  public void toFile(File path) {
+  }
+  
+  public String asString() {
+    try {
+      JsonDocument jsondoc = buildJsonDoc();
+      return JSON.std.with(JSON.Feature.PRETTY_PRINT_OUTPUT).asString(jsondoc);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not build JSON", ex);
+    }
+  }
+  
 }
