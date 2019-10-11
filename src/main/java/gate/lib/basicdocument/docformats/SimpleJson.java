5f4787c8b9eb99983a4e0924494b8e2698041730
Johann Petrak <johann.petrak@gmail.com>
2019-10-11 18:05:32 +0100

    WIP

---
+++ src/main/java/gate/lib/basicdocument/docformats/SimpleJson.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gate.lib.basicdocument.docformats;
+
+/**
+ *
+ * @author Johann Petrak <johann.petrak@gmail.com>
+ */
+public class SimpleJson {
+  
+}
