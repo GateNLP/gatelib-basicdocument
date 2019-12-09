59db699f7906f1dccf5eb595c3a0d87a8e847b65
Johann Petrak <johann.petrak@gmail.com>
2019-12-09 20:48:15 +0100

    Add license headers.

--- src/test/java/gate/lib/basicdocument/tests/TestBasicDocument1.java
+++ src/test/java/gate/lib/basicdocument/tests/TestBasicDocument1.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2019 The University of Sheffield.
+ *
+ * This file is part of gatelib-basicdocument 
+ * (see https://github.com/GateNLP/gatelib-basicdocument).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package gate.lib.basicdocument.tests;
 
 import gate.AnnotationSet;
@@ -30,6 +49,11 @@ public class TestBasicDocument1 extends TestCase {
     Gate.init();
   }
   
+  /**
+   * Run a test.
+   * @throws ResourceInstantiationException
+   * @throws InvalidOffsetException 
+   */
   @Test
   public void testBasic1() throws ResourceInstantiationException, InvalidOffsetException {
     // create a simple GATE document, convert to BdocDocument
